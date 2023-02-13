86311e0d6dd3af3dd2b4ded0db844946b4d67156
rockSugarL <alyssa00chen@outlook.com>
2023-02-13 12:31:57 +0800

    project init

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,15 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import fastClick from "fastclick";
+import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
+
+//路由就是根据网址的不同，返回不同的内容给用户
